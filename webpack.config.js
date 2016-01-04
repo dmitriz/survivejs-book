0d4ab7dc98542deabdd6f809ff91017338fe5ea8
dmitriz <dmitri14@gmail.com>
2016-01-04 02:47:44 +0000

    Clean web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-var path = require('path');
+//var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 
-const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
-};
+// const PATHS = {
+//   app: path.join(__dirname, 'app'),
+//   build: path.join(__dirname, 'build')
+// };
 
 module.exports = {
 
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
-  entry: PATHS.app,
+  entry: './app',
 
   // All outputs are in build
   output: {
-    path: PATHS.build,
+    path: './build',
     filename: 'bundle.js'
   },
   plugins: [
