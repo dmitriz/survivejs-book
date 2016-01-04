0d64a8a6a853069bffe5e96b90d1631132aa90e2
dmitriz <dmitri14@gmail.com>
2016-01-04 11:06:09 +0000

    Push data into App Class

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,25 +2,33 @@ import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
 
-
-const notesArray = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-];
-
-// Main App Component
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+
+  }
+
   render() {
+
+    const notesArray = this.state.notes;
 
 	  // Pass notes data as prop 
     return (
